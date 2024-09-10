527b4c398e0143b2dd8443adef8e9b82f2e09559
Aman verma <vermaaman2832003@gmail.com>
2024-09-10 23:57:09 +0530

    Created sign-up API route and middleware function for error Handling

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.router.js';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -13,7 +15,21 @@ mongoose
   });
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err,re,res,next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message
+    })
+})
