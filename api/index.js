0cfd71c8b20695f741d4ddf64d6a079046ac53e0
Aman verma <vermaaman2832003@gmail.com>
2024-10-24 02:21:02 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.router.js';
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import listingRouter from './routes/listing.route.js'
 dotenv.config();
 
 mongoose
@@ -27,8 +28,9 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-app.use((err,re,res,next) =>{
+app.use((err,req,res,next) =>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
     return res.status(statusCode).json({
